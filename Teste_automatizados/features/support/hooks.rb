107bd398cc9ad4de1103f3411702f65c3dcae9da
Matheus Santos <matheus.santos.x4@gmail.com>
2023-08-08 18:49:41 -0300

    Implementações: - adicionado context no hooks - Adicionado a criação de screenshots no after

--- Teste_automatizados/features/support/hooks.rb
+++ Teste_automatizados/features/support/hooks.rb
@@ -6,19 +6,21 @@ require 'cpf_cnpj'
 require 'webdrivers'
 require 'json'
 
-
-file = File.read('C:/Repositorios/producao_br/Projeto-automacao/Teste_automatizados/features/support/dados.json')
-data_hash = JSON.parse(file)
+context do
+  dados = File.new("./features/support/dados.json", "r")
+  file = File.read(dados)
+  $data_hash = JSON.parse(file)
+  $user = $data_hash['dados_user']['login']
+  $senha = $data_hash['dados_user']['senha']
+  Capybara.app_host = $data_hash['dados_user']['url']
+  $ambiente = $data_hash['dados_user']['ambiente']
+  $cookie_store = $data_hash['cookies'][0]['valor']
+  $cookie_kairos_sessionid = $data_hash['cookies'][1]['valor']
+  $cookie_aspnet = $data_hash['cookies'][2]['valor']
+end
 
 Before do
-  $user = data_hash['dados_user']['login']
-  $senha = data_hash['dados_user']['senha']
-  Capybara.app_host = data_hash['dados_user']['url']
-  @ambiente = data_hash['dados_user']['ambiente']
-  $cookie_store = data_hash['cookies'][0]['valor']
-  $cookie_kairos_sessionid = data_hash['cookies'][1]['valor']
-  $cookie_aspnet = data_hash['cookies'][2]['valor']
-
+  puts "seila"
   # Instanciando as Classes
   @login = Login.new
   @complements = Complements.new
@@ -27,11 +29,14 @@ Before do
   @obrigatorio = Obrigatorios.new
   @cadastros_relogios = Cadastros_relogios.new
   @resources = Resources.new
-
 end
 
 
 
 After do |scenario|
-
+  if scenario.failed?
+    path = "html-report/#{scenario.__id__}.html"
+    page.driver.browser.save_screenshot(path)
+    attach(path, "image/png")
+  end
 end
