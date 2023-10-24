ba427b118d3966ca2580fc9691619f2edfa886f0
Matheus Santos <matheus.santos.x4@gmail.com>
2023-10-24 15:14:16 -0300

    Implementações: - Entregando metodo para esperar o loading da pagina

--- Teste_automatizados/features/steps/BR/Kairos/Login/3-princ_login_geral.rb
+++ Teste_automatizados/features/steps/BR/Kairos/Login/3-princ_login_geral.rb
@@ -1,7 +1,6 @@
 Dado('que acesso a tela de login em {string} no ambiente BR') do |path|
     visit path
     # Realiza login
-    sleep 15
     @login.logon_ambiente_geral
     @validadores.validar_permanencia_pagina(path)
     @common.fechar_popup
